c00927154514b3ef6ecc9e72dadfcc2ebb68a2bc
Jose_Valbuena <jvalbuen@purdue.edu>
2020-01-30 16:59:54 -0500

    changed init message

--- src/App.js
+++ src/App.js
@@ -9,17 +9,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>
+          Welcome to Coexist!
+        </h2>
+        <h4>
+        Development in progress
+        </h4>
       </header>
     </div>
   );
