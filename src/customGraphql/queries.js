cfffedc7fd397e3796412cfb4f2c6eea078512b8
PKPurdue <pkrivack@purdue.edu>
2020-02-27 20:15:29 -0500

    group settings works

--- src/customGraphql/queries.js
+++ src/customGraphql/queries.js
@@ -25,3 +25,43 @@ export const getUser = `query GetUser($id: ID!) {
     }
   }
 }`;
+
+
+export const getGroup = `query GetGroup($id: ID!) {
+  getGroup(id: $id) {
+    id
+    name
+    owner {
+      id
+      username
+      email
+      name
+      phone
+      groups {
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+    type
+    description
+    users {
+      items {
+		  user {
+			   id
+		name
+		email
+		phone
+		createdAt
+		updatedAt
+		username
+		  }
+       
+      }
+    }
+    createdAt
+    updatedAt
+  }
+}
+`;
+
