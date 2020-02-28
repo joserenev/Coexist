fad275f36c7d8f0d3dc01e92d6ce795ff5b2b686
PKPurdue <pkrivack@purdue.edu>
2020-02-27 21:03:19 -0500

    added misc group data

--- src/components/Groups/EditGroupSettings.js
+++ src/components/Groups/EditGroupSettings.js
@@ -216,6 +216,9 @@ export default function EditGroupSettings({ groupID }: Props) {
 														defaultValue={groupData.description}
                                                     ></textarea>
                                                 </div>
+												<div>Owner: {groupData.owner.name}</div>
+												<div>Group type: {groupData.type}</div>
+												<div>Created at: {groupData.createdAt}</div>
                                             </div>
                                         </div>
                                         <div class="d-flex flex-row bd-highlight justify-content-around">
