72cab402174e1b5e357d50a1971ede01430fc483
dhriti04 <chawla5@purdue.edu>
2020-03-23 21:27:48 -0400

    UI for option to accept/reject reciept req

--- src/pages/Expenses/ExpensesReceiptRow.js
+++ src/pages/Expenses/ExpensesReceiptRow.js
@@ -18,6 +18,8 @@ import { makeStyles, useTheme } from "@material-ui/core/styles";
 import ReceiptIcon from "@material-ui/icons/Receipt";
 import Paper from "@material-ui/core/Paper";
 import ButtonBase from "@material-ui/core/ButtonBase";
+import AcceptIcon from "@material-ui/icons/CheckCircle";
+import RejectIcon from "@material-ui/icons/Cancel";
 
 // import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
 // import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
@@ -77,10 +79,12 @@ function ExpensesReceiptRow(): React.MixedElement {
                                 </Typography>
                             </Grid>
                             <Grid item>
-                                <Button variant="contained">
-                                    {" "}
-                                    <i>Click to pay</i>
-                                </Button>
+                                <ButtonBase>
+                                    <AcceptIcon />
+                                </ButtonBase>
+                                <ButtonBase>
+                                    <RejectIcon />
+                                </ButtonBase>
                             </Grid>
                         </Grid>
                         <Grid item>
