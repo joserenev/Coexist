598d9a1b6eeffe626d39df9c9fb4036ea7fca8ee
ggarciap <genaro.gp.03@gmail.com>
2020-03-29 11:43:28 -0400

    Adding create receipt button

--- src/pages/Expenses/ExpensesPage.js
+++ src/pages/Expenses/ExpensesPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 // <img src={logo} alt="logo" className={classes.logotypeImage} />
 import {
     Grid,
@@ -8,14 +8,18 @@ import {
     Tabs,
     Tab,
     TextField,
-    Fade
+    Fade,
+    Icon
 } from "@material-ui/core";
 
 import { withRouter } from "react-router-dom";
 import { withStyles } from "@material-ui/styles";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { makeStyles, useTheme, MuiThemeProvider,createMuiTheme} from "@material-ui/core/styles";
+import AddIcon from '@material-ui/icons/Add';
+import { green } from '@material-ui/core/colors';
 
 import ExpensesReceiptRow from "./ExpensesReceiptRow";
+import ExpenseCreate from "./ExpenseCreate";
 
 const useStyles = makeStyles(theme => ({
     headContainer: {
@@ -27,17 +31,45 @@ const useStyles = makeStyles(theme => ({
         justifyContent: "center",
         display: "flex",
         alignItems: "center"
-    }
+    },
+    submit: {
+        marginTop: theme.spacing.unit * 3,
+        marginLeft: '745px',
+        padding: '15px',
+        
+        
+    },
 }));
+
+const greenTheme = createMuiTheme({ palette: {     primary: green,
+    secondary: {
+      main: '#c8e6c9',
+    }, } })
 
 function ExpensesPage(): React.MixedElement {
     const classes = useStyles();
     const theme = useTheme();
+    const [isDialogOpen, setDialogOpen] = useState(false);
     return (
         <div className={classes.headContainer}>
+            <MuiThemeProvider theme={greenTheme}>
+                <Button
+                variant="contained"
+                color="secondary"
+                className={classes.submit}
+                endIcon={<AddIcon />}
+                onClick={() => {
+                    setDialogOpen(true);
+                }}
+                
+                >
+                    Add Receipt
+                </Button>
+            </MuiThemeProvider>
             <ExpensesReceiptRow />
             <ExpensesReceiptRow />
             <ExpensesReceiptRow />
+            <ExpenseCreate isDialogOpen={isDialogOpen} setDialogOpen={setDialogOpen} />
         </div>
     );
 }
