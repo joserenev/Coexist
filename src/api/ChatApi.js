410967a1c14f47e9a181d7f062b155e32fc78d8f
Jose_Valbuena <jvalbuen@purdue.edu>
2020-04-02 15:33:08 -0400

    separated group chats in the backend structure

--- src/api/ChatApi.js
+++ src/api/ChatApi.js
@@ -1,15 +1,15 @@
 import firebase from 'firebase';
 
-export async function sendMessage(sender, msg){
-  firebase.database().ref("messages").push().set({
+export async function sendMessage(groupID, sender, msg){
+  firebase.database().ref('Group/' + groupID).push().set({
     "sender": sender,
     "message": msg
   });
 }
 
 //This function listens for new messages from the db in realtime
-export async function listenerForMessages(){
-  firebase.database().ref("messages").on("child_added", function(snapshot) {
+export async function listenerForMessages(groupID){
+  firebase.database().ref("Group/").on("child_added", function(snapshot) {
     //snapshot.val().sender;
     //snapshot.val().message;
     //snapshot.key();
